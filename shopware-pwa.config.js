794eab8db875ba327dae97bcdb6898014ff9486e
mumtaz1000 <shereenfatima1000@gmail.com>
2022-02-24 09:31:29 +0100

    First step starting a new shopware pwa project

---
+++ shopware-pwa.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  shopwareEndpoint: "http://localhost",
+  shopwareAccessToken: "SWSCRNJ4TDNLV0NUT01YMENSCA",
+  shopwareDomainsAllowList: ["https://pwa-demo-api.shopware.com/trunk/", ]
+};
